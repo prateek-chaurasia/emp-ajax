fc1535d7c94671dfc4fb9bbc37d6ec030d863c3b
Prateek <escape2728@gmail.com>
2017-03-01 16:35:13 +0530

    Started writing basic unit test cases

--- myapp/functional_tests.py
+++ myapp/functional_tests.py
@@ -1,8 +1,31 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+#browser = webdriver.Firefox()
+#browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
-browser.close()
+#assert 'Django' in browser.title
+#browser.quit()
+
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
+		"""This function runs before and after each test """
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self):
+		"""This function runs before and after each test """
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		"""Function starts with name test_ is a test method.
+		And will run by the test runner. """
+		self.browser.get("http://localhost:8000/")
+		
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+	unittest.main()
+
+
